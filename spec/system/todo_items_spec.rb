e74f00f837319eb59fb01a35fafd0e889ba0466b
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-02-15 09:50:47 +0900

    TODO 項目を作成できるというテストを追加

--- spec/system/todo_items_spec.rb
+++ spec/system/todo_items_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe 'Todo Items', type: :system do
 
     expect(page).to have_content 'TODO item 10'
   end
+
+  it 'can create new todo item' do
+    create_list(:todo_item, 10, user: @user)
+
+    visit '/todo_items'
+
+    expect(page).to have_content 'TODO item 10'
+
+    click_button 'Create'
+    fill_in 'Name', with: 'A new TODO'
+    fill_in 'Body', with: 'This is new TODO description'
+    click_button 'Save'
+
+    expect(page).to have_content "A new TODO"
+    expect(page).not_to have_content 'This is new TODO description'
+    find('summary', text: 'A new TODO').click
+    expect(page).to have_content 'This is new TODO description'
+  end
 end
