2c2d8673cf2abf4142c3a79e8e1c12cf630d0273
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-02-15 09:07:19 +0900

    email の生成には Faker を使うことにした

--- Gemfile
+++ Gemfile
@@ -63,6 +63,7 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'database_rewinder'
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { 'test@example.com' }
+    email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
   end
