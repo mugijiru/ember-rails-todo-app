54d2daafadee00878e2e5a81aaad05aa23126b2f
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-02-15 09:36:10 +0900

    system spec 用に Devise のテスト設定を追加
    ついでに spec/support 内に移動した

---
+++ spec/support/devise.rb
@@ -0,0 +1,8 @@
+RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Warden::Test::Helpers, type: :system
+
+  config.after(:each) do
+    Warden.test_reset!
+  end
+end
