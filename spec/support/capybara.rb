6c92dca2f71d7b7ec760b3e23478bbf8e15e8bb0
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-02-20 22:45:31 +0900

    Capybara で諸々設定追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -13,13 +13,24 @@ if ENV['CI'] == 'true'
     end
   end
 else
+
+  Capybara.register_driver :remote_chrome do |app|
+    url = ENV.fetch('SELENIUM_REMOTE_URL')
+    caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+      'goog:chromeOptions' => {
+        'args' => [
+          'no-sandbox',
+          'headless',
+          'disable-gpu'
+        ]
+      }
+    )
+    Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
+  end
+
   RSpec.configure do |config|
     config.before(:each, type: :system) do
-      driven_by :selenium, options: {
-        browser: :remote,
-        url: ENV.fetch('SELENIUM_REMOTE_URL'),
-        desired_capabilities: :chrome
-      }
+      driven_by :remote_chrome
       Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
       Capybara.server_port = 3000
       Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
