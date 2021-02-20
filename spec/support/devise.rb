289a61bcb7d322d4cebd8d66d02005e042d8a026
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-02-20 16:22:55 +0900

    routing のテスト修正

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,6 +1,7 @@
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Warden::Test::Helpers, type: :system
+  config.include Warden::Test::Helpers, type: :routing
 
   config.after(:each) do
     Warden.test_reset!
