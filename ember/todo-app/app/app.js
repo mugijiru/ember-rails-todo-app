da37e4342c120a4c73a88f7b72ee7344d07eb3e6
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-04-30 00:19:04 +0900

    v3.12.1...v3.16.2

--- ember/todo-app/app/app.js
+++ ember/todo-app/app/app.js
@@ -1,17 +1,13 @@
 import Application from '@ember/application';
-import Resolver from './resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const TodoApp = Application.extend({
-  rootElement: '#todo-app',
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  locationType: config.locationType,
-  rootUrl: config.rootUrl,
-  Resolver
-});
+export default class App extends Application {
+  rootElement = '#todo-app';
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 
-loadInitializers(TodoApp, config.modulePrefix);
-
-export default TodoApp;
+loadInitializers(App, config.modulePrefix);
