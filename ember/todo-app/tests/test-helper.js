a38df47a04c09876edab068adfd1a80ffcc703c4
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-01 21:34:54 +0900

    v3.16.2...v3.20.2

--- ember/todo-app/tests/test-helper.js
+++ ember/todo-app/tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'todo-app/app';
+import config from 'todo-app/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
