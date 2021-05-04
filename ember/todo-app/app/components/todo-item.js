08780f169d7276f4f2a225fee5e66989401c2017
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-04 23:56:32 +0900

    todo-items component も新しい形式に変更

--- ember/todo-app/app/components/todo-item.js
+++ ember/todo-app/app/components/todo-item.js
@@ -1,43 +1,46 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { later } from '@ember/runloop';
 import { computed, action } from '@ember/object';
-import Component from '@ember/component';
 
-export default Component.extend({
-  tagName: 'li',
-  classNames: ['p-todo-item'],
-  classNameBindings: ['isCompleted:p-todo-item__completed'],
+export default class TodoItem extends Component {
+  @tracked isShowing = false
 
-  item: null,
-  isCompleted: computed.reads('item.isCompleted'),
+  constructor () {
+    super(...arguments)
+    this.isShowing = true
+    later(() => { this.isShowing = false }, 10)
+  }
 
-  didInsertElement() {
-    this._super(...arguments);
-    this.element.classList.add('p-todo-item--showing-enter');
-  },
+  get item() { return this.args.item ?? null }
 
-  didRender() {
-    this._super(...arguments);
-    later(() => {
-      this.element.classList.remove('p-todo-item--showing-enter');
-    }, 10);
-  },
+  get isCompleted() {
+    if (!this.item) { return false }
+    return this.item.isCompleted
+  }
+
+  get classNamesString() {
+    const classList = ['p-todo-item']
+    if (this.isCompleted) { classList.push('p-todo-item__completed') }
+    if (this.isShowing) { classList.push('p-todo-item--showing-enter') }
+    return classList.join(' ')
+  }
 
   @action
   toggle() {
     const item = this.item;
     item.set('isCompleted', !item.get('isCompleted'));
     item.save();
-  },
+  }
 
   @action
   edit() {
     const item = this.item;
-    this.setEditingRecord(item);
-  },
+    this.args.setEditingRecord(item);
+  }
 
   @action
   delete() {
-    const item = this.item;
-    item.destroyRecord();
-  },
-});
+    this.item.destroyRecord();
+  }
+}
