fe1cf244bbc47094fde4f7502c73d1506ed2bf93
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-05 11:14:04 +0900

    tracked は使ってなかったので import をやめた

--- ember/todo-app/app/components/todo-item/form-modal.js
+++ ember/todo-app/app/components/todo-item/form-modal.js
@@ -1,6 +1,5 @@
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+import Component from '@glimmer/component'
+import { action } from '@ember/object'
 
 export default class TodoItemFormModal extends Component {
   get item() { return this.args.item ?? null }
