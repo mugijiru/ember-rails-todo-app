0c48dafb6743c678b8e9dea9a457ac0b69058e6b
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-15 23:45:29 +0900

    replace double qoute to single quote

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :todo_items, only: :index
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   root 'todo_items#index'
 
   namespace :api do
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     end
   end
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   mount_ember_app :todo_app, to: '/todo_items', controller: 'todo_items', action: 'index'
 end
