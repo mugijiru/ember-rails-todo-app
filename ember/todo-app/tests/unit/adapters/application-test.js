d255b4f3a9f944437ab022e4c2b16ade1e26a5cf
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-08-26 23:16:03 +0900

    application adapter のテストを追加
    最近更新したコードなのと
    Unit Test として書き易そうなところだったので
    application adapter だけとりあえずテストを書いた
    
    今後 Coverage を取れるようにした上で
    他のコードのテストも書こうと考えている

---
+++ ember/todo-app/tests/unit/adapters/application-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Adapter | application', function(hooks) {
+  setupTest(hooks);
+
+  test('should namespace equal "api/v1"', function(assert) {
+    const adapter = this.owner.lookup('adapter:application');
+    assert.equal(adapter.namespace, 'api/v1');
+  });
+
+  test('pathForType でケバブケースはスネークケースにされた上に複数形にされる', function(assert) {
+    const adapter = this.owner.lookup('adapter:application');
+    assert.equal(adapter.pathForType('kebab-case'), 'kebab_cases');
+  });
+});
