1b9b550b9c5bd49f97a2424254f038e953313550
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-05-28 16:32:32 +0900

    fix Use service instead of inject

--- ember/todo-app/app/controllers/todo-items.js
+++ ember/todo-app/app/controllers/todo-items.js
@@ -1,6 +1,6 @@
 import { later } from '@ember/runloop'
 import { action } from '@ember/object'
-import { inject as service } from '@ember/service'
+import { service } from '@ember/service'
 import Controller from '@ember/controller'
 import { tracked } from '@glimmer/tracking'
 
