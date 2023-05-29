351b457baf6550bf864b3095a744f2f73893d1e6
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-05-29 09:04:10 +0900

    style: Use let instead of instance variables
    rubocop 先生に怒られたので修正。

--- spec/requests/api/v1/todo_items_spec.rb
+++ spec/requests/api/v1/todo_items_spec.rb
@@ -13,16 +13,14 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe '/todo_items', type: :request do
-  before do
-    @user = create(:user)
-    sign_in @user
-  end
-
+  let(:user) { create(:user) }
   let(:request_headers) { { 'CONTENT_TYPE' => 'application/vnd.api+json' } }
+
+  before { sign_in user }
 
   describe 'GET /index' do
     it 'renders a successful response' do
-      create(:todo_item, name: 'valid name', user: @user)
+      create(:todo_item, name: 'valid name', user: user)
       get '/api/v1/todo_items'
       json = JSON.parse(response.body)
       expect(json['data'][0]['attributes']['name']).to eq 'valid name'
@@ -63,7 +61,7 @@ RSpec.describe '/todo_items', type: :request do
   describe 'PATCH /update' do
     context 'with valid parameters' do
       it 'updates the requested todo_item' do
-        todo_item = create(:todo_item, name: 'existing item', user: @user)
+        todo_item = create(:todo_item, name: 'existing item', user: user)
         patch "/api/v1/todo_items/#{todo_item.id}",
               headers: request_headers,
               params: generate_params(name: 'edited name')
@@ -77,7 +75,7 @@ RSpec.describe '/todo_items', type: :request do
 
     context 'with invalid parameters' do
       it 'response status is 422(unprocessable entity)' do
-        todo_item = create(:todo_item, name: 'TODO item', user: @user)
+        todo_item = create(:todo_item, name: 'TODO item', user: user)
         patch "/api/v1/todo_items/#{todo_item.id}",
               headers: request_headers,
               params: generate_params(name: nil)
@@ -88,14 +86,14 @@ RSpec.describe '/todo_items', type: :request do
 
   describe 'DELETE /destroy' do
     it 'destroys the requested todo_item' do
-      todo_item = create(:todo_item, name: 'TODO item', user: @user)
+      todo_item = create(:todo_item, name: 'TODO item', user: user)
       expect {
         delete "/api/v1/todo_items/#{todo_item.id}"
       }.to change(TodoItem, :count).by(-1)
     end
 
     it 'response status is 204(no content)' do
-      todo_item = create(:todo_item, name: 'TODO item', user: @user)
+      todo_item = create(:todo_item, name: 'TODO item', user: user)
       delete "/api/v1/todo_items/#{todo_item.id}"
       expect(response.status).to eq(204)
     end
