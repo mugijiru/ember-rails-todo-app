c22b5e1f82822eeae8c7496ac923740c7263e8b4
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-02 22:59:01 +0900

    chore: Setup ember-cli-code-coverage
    ```
    docker-compose -f docker-compose.dev.yml run -e COVERAGE=true rails bash -c 'cd ember/todo-app && yarn test'
    ```
    
    で ember/todo-app/coverage にカバレッジが出力されるようにした

--- ember/todo-app/tests/test-helper.js
+++ ember/todo-app/tests/test-helper.js
@@ -4,6 +4,15 @@ import * as QUnit from 'qunit'
 import { setApplication } from '@ember/test-helpers'
 import { setup } from 'qunit-dom'
 import { start } from 'ember-qunit'
+import {
+  forceModulesToBeLoaded,
+  sendCoverage,
+} from 'ember-cli-code-coverage/test-support'
+
+QUnit.done(async () => {
+  forceModulesToBeLoaded()
+  await sendCoverage()
+})
 
 setApplication(Application.create(config.APP))
 
