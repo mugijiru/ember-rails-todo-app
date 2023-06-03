a02bb3c1bfe2d51cb0c519218f1623360d3d3924
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-03 23:02:30 +0900

    style: Translate routes/todo-items to typescript
    TODO: setupController の引数調整

--- ember/todo-app/app/routes/todo-items.ts
+++ ember/todo-app/app/routes/todo-items.ts
@@ -1,14 +1,16 @@
 import Route from '@ember/routing/route'
 import { service } from '@ember/service'
+import DS from 'ember-data'
 
 export default class TodoItem extends Route {
-  @service store
+  @service
+  declare store: DS.Store
 
   model() {
     return this.store.findAll('todo-item')
   }
 
-  setupController(controller, model) {
+  setupController(controller: any, model: any) {
     controller.set('todoItems', model)
   }
 }
