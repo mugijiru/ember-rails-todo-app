3a394d66630ff08c4d20af43730e0b3807eebfab
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-05 07:47:21 +0900

    test: Improve ember-cli-mirage config

--- ember/todo-app/mirage/config.js
+++ ember/todo-app/mirage/config.js
@@ -1,26 +1,15 @@
-export default function() {
+import { discoverEmberDataModels } from "ember-cli-mirage";
+import { createServer } from 'miragejs';
 
-  // These comments are here to help you get started. Feel free to delete them.
+export default function(config) {
+  let finalConfig = {
+    ...config,
+    models: { ...discoverEmberDataModels(), ...config.models },
+    routes() {
+      this.namespace = '/api/v1'
+      this.resource('todo-item')
+    },
+  };
 
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
+  return createServer(finalConfig);
 }
