6cc7268bb9e8c89780fa94fe42ceedeafb040160
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-05 07:48:05 +0900

    test: Replace msw to mirage

--- ember/todo-app/tests/unit/routes/todo-items-test.ts
+++ ember/todo-app/tests/unit/routes/todo-items-test.ts
@@ -1,65 +1,49 @@
 import { assert, module, test } from 'qunit'
-import { setupWorker, rest } from 'msw'
 import { TestContext } from '@ember/test-helpers'
 import { setupTest } from 'todo-app/tests/helpers'
 import TodoItemModel from 'todo-app/models/todo-item'
 import TodoItemsRoute from 'todo-app/routes/todo-items'
+import { Server } from 'miragejs';
+import { setupMirage } from 'ember-cli-mirage/test-support'
 
-interface Context extends TestContext {
-  worker: ReturnType<typeof setupWorker>
+interface MirageTestContext extends TestContext {
+  server: Server;
 }
 
 module('Unit | Route | todo-items', function (hooks) {
   setupTest(hooks)
+  setupMirage(hooks)
 
-  hooks.before(async function (this: Context) {
-    this.worker = setupWorker()
-    await this.worker.start({ onUnhandledRequest: 'error', quiet: true })
-  })
-
-  hooks.afterEach(function (this: Context) {
-    this.worker.resetHandlers()
-  })
-
-  hooks.after(function (this: Context) {
-    this.worker.stop()
-  })
-
-  test('the model is stored all todo items', async function (this: Context) {
+  test('the model is stored all todo items', async function (this: MirageTestContext) {
     const route = this.owner.lookup('route:todo-items') as any // eslint-disable-line @typescript-eslint/no-explicit-any
 
-    this.worker.use(
-      rest.get('/api/v1/todo_items', (_req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.set('Content-Type', 'application/vnd.api+json'),
-          ctx.json({
-            data: [
-              {
-                type: 'todo-item',
-                id: 1,
-                attributes: {
-                  name: 'item 1',
-                  is_completed: false,
-                  created_at: '2021-01-01T00:00:00.000Z',
-                  updated_at: '2021-01-01T00:00:00.000Z',
-                },
-              },
-              {
-                type: 'todo-item',
-                id: 2,
-                attributes: {
-                  name: 'item 2',
-                  is_completed: false,
-                  created_at: '2021-01-01T00:00:00.000Z',
-                  updated_at: '2021-01-01T00:00:00.000Z',
-                },
-              },
-            ],
-          })
-        )
-      })
-    )
+    this.server.get('/todo_items', () => {
+      return {
+        data: [
+          {
+            type: 'todo-item',
+            id: 1,
+            attributes: {
+              name: 'item 1',
+              is_completed: false,
+              created_at: '2021-01-01T00:00:00.000Z',
+              updated_at: '2021-01-01T00:00:00.000Z',
+            },
+          },
+          {
+            type: 'todo-item',
+            id: 2,
+            attributes: {
+              name: 'item 2',
+              is_completed: false,
+              created_at: '2021-01-01T00:00:00.000Z',
+              updated_at: '2021-01-01T00:00:00.000Z',
+            },
+          },
+        ],
+      }
+    })
+
     const todoItems = (await route.model()) as TodoItemModel[]
     assert.deepEqual(
       todoItems.map((i) => i.name),
