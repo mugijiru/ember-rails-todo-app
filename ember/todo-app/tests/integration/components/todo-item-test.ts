c0dc7112a48e3aaa7b89e201e6049759d921317f
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-10 23:33:34 +0900

    test: Add completion state test for components/todo-item

--- ember/todo-app/tests/integration/components/todo-item-test.ts
+++ ember/todo-app/tests/integration/components/todo-item-test.ts
@@ -39,4 +39,64 @@ module('Integrations | Coomponents | todo-item', function (hooks) {
     const dom = assert.dom('.p-todo-item')
     dom.hasTextContaining('new item!')
   })
+
+  module('completion state', function () {
+    test('rendered item is not completed when passed incompleted item', async function (this: Context, assert) {
+      const store = this.owner.lookup('service:store')
+      store.push({
+        data: [
+          {
+            id: '1',
+            type: 'todo-item',
+            attributes: {
+              name: 'new item!',
+              isCompleted: false,
+            },
+          },
+        ],
+      })
+
+      this.item = store.peekRecord('todo-item', 1)
+      this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+      await render(
+        hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
+      )
+
+      const dom = assert.dom('.p-todo-item')
+      dom.hasNoClass('p-todo-item__completed')
+
+      const checkButton = assert.dom('.p-todo-item button:first-of-type')
+      checkButton.hasNoClass('mg-checkbox--checked')
+    })
+
+    test('rendered item is completed when passed completed item', async function (this: Context, assert) {
+      const store = this.owner.lookup('service:store')
+      store.push({
+        data: [
+          {
+            id: '3',
+            type: 'todo-item',
+            attributes: {
+              name: 'new item!',
+              isCompleted: true,
+            },
+          },
+        ],
+      })
+
+      this.item = store.peekRecord('todo-item', 3)
+      this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+      await render(
+        hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
+      )
+
+      const dom = assert.dom('.p-todo-item')
+      dom.hasClass('p-todo-item__completed')
+
+      const checkButton = assert.dom('.p-todo-item button:first-of-type')
+      checkButton.hasClass('mg-checkbox--checked')
+    })
+  })
 })
