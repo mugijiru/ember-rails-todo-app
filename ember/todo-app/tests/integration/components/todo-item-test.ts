e185b748468064c3ce86e978d7a927ff843f2c52
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-10 23:13:33 +0900

    test: Add simple test for components/todo-item

---
+++ ember/todo-app/tests/integration/components/todo-item-test.ts
@@ -0,0 +1,42 @@
+import { render, TestContext } from '@ember/test-helpers'
+import { hbs } from 'ember-cli-htmlbars'
+import { module, test } from 'qunit'
+
+import TodoItemModel from 'todo-app/models/todo-item'
+
+import { setupRenderingTest } from '../../helpers'
+
+interface Context extends TestContext {
+  item: TodoItemModel | null
+  setEditingRecord: (item: TodoItemModel) => void
+}
+
+module('Integrations | Coomponents | todo-item', function (hooks) {
+  setupRenderingTest(hooks)
+
+  test('renders item name', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    store.push({
+      data: [
+        {
+          id: '1',
+          type: 'todo-item',
+          attributes: {
+            name: 'new item!',
+            isCompleted: false,
+          },
+        },
+      ],
+    })
+
+    this.item = store.peekRecord('todo-item', 1)
+    this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+    await render(
+      hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
+    )
+
+    const dom = assert.dom('.p-todo-item')
+    dom.hasTextContaining('new item!')
+  })
+})
