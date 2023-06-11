68a50640a5adabe26e64693fdada70565c8a246c
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-11 15:20:44 +0900

    test: Add test for title

--- ember/todo-app/tests/integration/components/todo-item/form-modal-test.ts
+++ ember/todo-app/tests/integration/components/todo-item/form-modal-test.ts
@@ -23,4 +23,43 @@ module("Integration | Component | todo-item/form-modal", function (hooks) {
     assert.dom('.modal input').hasValue('new item!');
     assert.dom('.modal textarea').hasValue('new item body!');
   })
+
+  test('rendered title is "New TODO when the item is new record"', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    const item = store.createRecord('todo-item', { name: 'new item!' })
+    this.item = item
+    this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+
+    assert.dom('.modal').hasTextContaining('New TODO');
+    assert.dom('.modal').doesNotContainText('Edit TODO');
+  })
+
+  test('rendered title is "New TODO when the item is new record"', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    store.push({
+      data: [
+        {
+          id: '1',
+          type: 'todo-item',
+          attributes: {
+            name: 'new item!',
+            body: 'new item body!',
+            isCompleted: false,
+          },
+        },
+      ],
+    })
+
+    const item = store.peekRecord('todo-item', 1)
+    if (item === null) {
+      throw new Error('item is null')
+    }
+    this.item = item
+    this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+
+    assert.dom('.modal').doesNotContainText('New TODO');
+    assert.dom('.modal').hasTextContaining('Edit TODO');
+  })
 })
