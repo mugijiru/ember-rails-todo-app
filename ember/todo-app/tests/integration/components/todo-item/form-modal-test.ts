c804b40e3e4e2d2773f3f5518212d976ffdc6cc2
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-11 16:38:26 +0900

    test: Add test for save item

--- ember/todo-app/tests/integration/components/todo-item/form-modal-test.ts
+++ ember/todo-app/tests/integration/components/todo-item/form-modal-test.ts
@@ -1,17 +1,32 @@
-import { find, findAll, render, TestContext } from "@ember/test-helpers";
+import { click, findAll, render, TestContext } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { module, test } from "qunit";
 import { setupRenderingTest } from "todo-app/tests/helpers";
 
 import TodoItemModel from "todo-app/models/todo-item";
+import { rest, setupWorker } from "msw";
 
 interface Context extends TestContext {
   item?: TodoItemModel;
   close: () => void;
+  worker: ReturnType<typeof setupWorker>
 }
 
 module("Integration | Component | todo-item/form-modal", function (hooks) {
   setupRenderingTest(hooks)
+
+  hooks.before(async function (this: Context) {
+    this.worker = setupWorker()
+    await this.worker.start({ onUnhandledRequest: 'error', quiet: true })
+  })
+
+  hooks.afterEach(function (this: Context) {
+    this.worker.resetHandlers()
+  })
+
+  hooks.after(function (this: Context) {
+    this.worker.stop()
+  })
 
   test('does not show modal when item is undefined', async function (this: Context, assert) {
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
@@ -78,5 +93,46 @@ module("Integration | Component | todo-item/form-modal", function (hooks) {
 
     assert.dom('.modal').doesNotContainText('New TODO');
     assert.dom('.modal').hasTextContaining('Edit TODO');
+  })
+
+  test('Save record when save button clicked', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    const item = store.createRecord('todo-item', { name: 'new item!', body: 'new item body!' })
+    this.item = item
+    this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+
+    assert.dom('.modal').isVisible();
+    const buttons = findAll('.modal button')
+    const saveButton = buttons.find((button) => button.textContent?.trim() === 'Save')
+    if (!saveButton) {
+      throw new Error('saveButton is null')
+    }
+
+    this.worker.use(
+      rest.post('/api/v1/todo_items', (_req, res, ctx) => {
+        return res.once(
+          ctx.status(201),
+          ctx.set('Content-Type', 'application/vnd.api+json'),
+          ctx.json({
+            data:              {
+                type: 'todo-item',
+                id: 1,
+                attributes: {
+                  name: 'new item!',
+                  is_completed: true,
+                  created_at: '2021-01-01T00:00:00.000Z',
+                  updated_at: '2021-01-01T00:00:00.000Z',
+                },
+              },
+          })
+        )
+      })
+    )
+
+    await click(saveButton)
+
+    const saveditem = store.peekRecord('todo-item', 1)
+    assert.ok(saveditem)
   })
 })
