f496e2cd6677b0d9e5ee5337f09e0bbcba285d4d
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-11 10:58:58 +0900

    test: Refactor components/todo-item-test
    msw の worker の Setup 等は before などに任せたいので
    リファクタリングを行った。
    
    その際 module を nest していると
    context がうまく引き継がれないので
    仕方なく nest を解除している

--- ember/todo-app/tests/integration/components/todo-item-test.ts
+++ ember/todo-app/tests/integration/components/todo-item-test.ts
@@ -10,10 +10,24 @@ import { setupRenderingTest } from '../../helpers'
 interface Context extends TestContext {
   item: TodoItemModel | null
   setEditingRecord: (item: TodoItemModel) => void
+  worker: ReturnType<typeof setupWorker>
 }
 
 module('Integrations | Coomponents | todo-item', function (hooks) {
   setupRenderingTest(hooks)
+
+  hooks.before(async function (this: Context) {
+    this.worker = setupWorker()
+    await this.worker.start({ onUnhandledRequest: 'error', quiet: true })
+  })
+
+  hooks.afterEach(function (this: Context) {
+    this.worker.resetHandlers()
+  })
+
+  hooks.after(function (this: Context) {
+    this.worker.stop()
+  })
 
   test('renders item name', async function (this: Context, assert) {
     const store = this.owner.lookup('service:store')
@@ -41,158 +55,150 @@ module('Integrations | Coomponents | todo-item', function (hooks) {
     dom.hasTextContaining('new item!')
   })
 
-  module('completion state', function () {
-    test('rendered item is not completed when passed incompleted item', async function (this: Context, assert) {
-      const store = this.owner.lookup('service:store')
-      store.push({
-        data: [
-          {
-            id: '1',
-            type: 'todo-item',
-            attributes: {
-              name: 'new item!',
-              isCompleted: false,
-            },
+  test('rendered item is not completed when passed incompleted item', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    store.push({
+      data: [
+        {
+          id: '1',
+          type: 'todo-item',
+          attributes: {
+            name: 'new item!',
+            isCompleted: false,
           },
-        ],
-      })
-
-      this.item = store.peekRecord('todo-item', 1)
-      this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-
-      await render(
-        hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
-      )
-
-      const dom = assert.dom('.p-todo-item')
-      dom.hasNoClass('p-todo-item__completed')
-
-      const checkButton = assert.dom('.p-todo-item button:first-of-type')
-      checkButton.hasNoClass('mg-checkbox--checked')
+        },
+      ],
     })
 
-    test('rendered item is completed when passed completed item', async function (this: Context, assert) {
-      const store = this.owner.lookup('service:store')
-      store.push({
-        data: [
-          {
-            id: '3',
-            type: 'todo-item',
-            attributes: {
-              name: 'new item!',
-              isCompleted: true,
-            },
+    this.item = store.peekRecord('todo-item', 1)
+    this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+    await render(
+      hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
+    )
+
+    const dom = assert.dom('.p-todo-item')
+    dom.hasNoClass('p-todo-item__completed')
+
+    const checkButton = assert.dom('.p-todo-item button:first-of-type')
+    checkButton.hasNoClass('mg-checkbox--checked')
+  })
+
+  test('rendered item is completed when passed completed item', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    store.push({
+      data: [
+        {
+          id: '3',
+          type: 'todo-item',
+          attributes: {
+            name: 'new item!',
+            isCompleted: true,
           },
-        ],
-      })
-
-      this.item = store.peekRecord('todo-item', 3)
-      this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-
-      await render(
-        hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
-      )
-
-      const dom = assert.dom('.p-todo-item')
-      dom.hasClass('p-todo-item__completed')
-
-      const checkButton = assert.dom('.p-todo-item button:first-of-type')
-      checkButton.hasClass('mg-checkbox--checked')
+        },
+      ],
     })
 
-    test('toggle completion state', async function (this: Context, assert) {
-      const store = this.owner.lookup('service:store')
-      store.push({
-        data: [
-          {
-            id: '1',
-            type: 'todo-item',
-            attributes: {
-              name: 'new item!',
-              isCompleted: false,
-            },
+    this.item = store.peekRecord('todo-item', 3)
+    this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+    await render(
+      hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
+    )
+
+    const dom = assert.dom('.p-todo-item')
+    dom.hasClass('p-todo-item__completed')
+
+    const checkButton = assert.dom('.p-todo-item button:first-of-type')
+    checkButton.hasClass('mg-checkbox--checked')
+  })
+
+  test('toggle completion state', async function (this: Context, assert) {
+    const store = this.owner.lookup('service:store')
+    store.push({
+      data: [
+        {
+          id: '1',
+          type: 'todo-item',
+          attributes: {
+            name: 'new item!',
+            isCompleted: false,
           },
-        ],
-      })
+        },
+      ],
+    })
 
-      const worker = setupWorker()
-      await worker.start({ onUnhandledRequest: 'error', quiet: true })
+    this.item = store.peekRecord('todo-item', 1)
+    this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
 
-      this.item = store.peekRecord('todo-item', 1)
-      this.setEditingRecord = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+    await render(
+      hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
+    )
 
-      await render(
-        hbs`<TodoItem @item={{this.item}} @setEditingRecord={{this.setEditingRecord}} />`
-      )
+    const dom = assert.dom('.p-todo-item')
+    dom.hasNoClass('p-todo-item__completed')
 
-      const dom = assert.dom('.p-todo-item')
-      dom.hasNoClass('p-todo-item__completed')
+    const checkButton = assert.dom('.p-todo-item button:first-of-type')
+    checkButton.hasNoClass('mg-checkbox--checked')
 
-      const checkButton = assert.dom('.p-todo-item button:first-of-type')
-      checkButton.hasNoClass('mg-checkbox--checked')
-
-      worker.use(
-        rest.patch('/api/v1/todo_items/1', (_req, res, ctx) => {
-          return res.once(
-            ctx.status(200),
-            ctx.set('Content-Type', 'application/vnd.api+json'),
-            ctx.json({
-              data: [
-                {
-                  type: 'todo-item',
-                  id: 1,
-                  attributes: {
-                    name: 'new item!',
-                    is_completed: true,
-                    created_at: '2021-01-01T00:00:00.000Z',
-                    updated_at: '2021-01-01T00:00:00.000Z',
-                  },
+    this.worker.use(
+      rest.patch('/api/v1/todo_items/1', (_req, res, ctx) => {
+        return res.once(
+          ctx.status(200),
+          ctx.set('Content-Type', 'application/vnd.api+json'),
+          ctx.json({
+            data: [
+              {
+                type: 'todo-item',
+                id: 1,
+                attributes: {
+                  name: 'new item!',
+                  is_completed: true,
+                  created_at: '2021-01-01T00:00:00.000Z',
+                  updated_at: '2021-01-01T00:00:00.000Z',
                 },
-              ],
-            })
-          )
-        })
-      )
+              },
+            ],
+          })
+        )
+      })
+    )
 
-      await click('.p-todo-item button:first-of-type')
+    await click('.p-todo-item button:first-of-type')
 
-      assert.dom('.p-todo-item').hasClass('p-todo-item__completed')
-      assert
-        .dom('.p-todo-item button:first-of-type')
-        .hasClass('mg-checkbox--checked')
+    assert.dom('.p-todo-item').hasClass('p-todo-item__completed')
+    assert
+      .dom('.p-todo-item button:first-of-type')
+      .hasClass('mg-checkbox--checked')
 
-      worker.use(
-        rest.patch('/api/v1/todo_items/1', (_req, res, ctx) => {
-          return res.once(
-            ctx.status(200),
-            ctx.set('Content-Type', 'application/vnd.api+json'),
-            ctx.json({
-              data: [
-                {
-                  type: 'todo-item',
-                  id: 1,
-                  attributes: {
-                    name: 'new item!',
-                    is_completed: false,
-                    created_at: '2021-01-01T00:00:00.000Z',
-                    updated_at: '2021-01-01T00:00:00.000Z',
-                  },
+    this.worker.use(
+      rest.patch('/api/v1/todo_items/1', (_req, res, ctx) => {
+        return res.once(
+          ctx.status(200),
+          ctx.set('Content-Type', 'application/vnd.api+json'),
+          ctx.json({
+            data: [
+              {
+                type: 'todo-item',
+                id: 1,
+                attributes: {
+                  name: 'new item!',
+                  is_completed: false,
+                  created_at: '2021-01-01T00:00:00.000Z',
+                  updated_at: '2021-01-01T00:00:00.000Z',
                 },
-              ],
-            })
-          )
-        })
-      )
+              },
+            ],
+          })
+        )
+      })
+    )
 
-      await click('.p-todo-item button:first-of-type')
+    await click('.p-todo-item button:first-of-type')
 
-      assert.dom('.p-todo-item').hasNoClass('p-todo-item__completed')
-      assert
-        .dom('.p-todo-item button:first-of-type')
-        .hasNoClass('mg-checkbox--checked')
-
-      worker.resetHandlers()
-      worker.stop()
-    })
+    assert.dom('.p-todo-item').hasNoClass('p-todo-item__completed')
+    assert
+      .dom('.p-todo-item button:first-of-type')
+      .hasNoClass('mg-checkbox--checked')
   })
 })
