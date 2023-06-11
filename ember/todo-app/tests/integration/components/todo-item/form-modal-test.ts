a59afd5b7de12d813f5063a2a81c9c38fad7fb6a
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-06-11 16:41:49 +0900

    style: RUN yarn run lint:fix

--- ember/todo-app/tests/integration/components/todo-item/form-modal-test.ts
+++ ember/todo-app/tests/integration/components/todo-item/form-modal-test.ts
@@ -1,18 +1,18 @@
-import { click, findAll, render, TestContext } from "@ember/test-helpers";
-import { hbs } from "ember-cli-htmlbars";
-import { module, test } from "qunit";
-import { setupRenderingTest } from "todo-app/tests/helpers";
+import { click, findAll, render, TestContext } from '@ember/test-helpers'
+import { hbs } from 'ember-cli-htmlbars'
+import { rest, setupWorker } from 'msw'
+import { module, test } from 'qunit'
 
-import TodoItemModel from "todo-app/models/todo-item";
-import { rest, setupWorker } from "msw";
+import TodoItemModel from 'todo-app/models/todo-item'
+import { setupRenderingTest } from 'todo-app/tests/helpers'
 
 interface Context extends TestContext {
-  item?: TodoItemModel;
-  close: () => void;
+  item?: TodoItemModel
+  close: () => void
   worker: ReturnType<typeof setupWorker>
 }
 
-module("Integration | Component | todo-item/form-modal", function (hooks) {
+module('Integration | Component | todo-item/form-modal', function (hooks) {
   setupRenderingTest(hooks)
 
   hooks.before(async function (this: Context) {
@@ -30,30 +30,42 @@ module("Integration | Component | todo-item/form-modal", function (hooks) {
 
   test('does not show modal when item is undefined', async function (this: Context, assert) {
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+    await render(
+      hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`)
+    )
 
-    assert.dom('.modal').isNotVisible();
+    assert.dom('.modal').isNotVisible()
   })
 
   test('show modal when item is exists', async function (this: Context, assert) {
     const store = this.owner.lookup('service:store')
-    const item = store.createRecord('todo-item', { name: 'new item!', body: 'new item body!' })
+    const item = store.createRecord('todo-item', {
+      name: 'new item!',
+      body: 'new item body!',
+    })
     this.item = item
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+    await render(
+      hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`)
+    )
 
-    assert.dom('.modal').isVisible();
+    assert.dom('.modal').isVisible()
   })
 
   test('renders form with item properties', async function (this: Context, assert) {
     const store = this.owner.lookup('service:store')
-    const item = store.createRecord('todo-item', { name: 'new item!', body: 'new item body!' })
+    const item = store.createRecord('todo-item', {
+      name: 'new item!',
+      body: 'new item body!',
+    })
     this.item = item
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+    await render(
+      hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`)
+    )
 
-    assert.dom('.modal input').hasValue('new item!');
-    assert.dom('.modal textarea').hasValue('new item body!');
+    assert.dom('.modal input').hasValue('new item!')
+    assert.dom('.modal textarea').hasValue('new item body!')
   })
 
   test('rendered title is "New TODO when the item is new record"', async function (this: Context, assert) {
@@ -61,13 +73,15 @@ module("Integration | Component | todo-item/form-modal", function (hooks) {
     const item = store.createRecord('todo-item', { name: 'new item!' })
     this.item = item
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+    await render(
+      hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`)
+    )
 
-    assert.dom('.modal').hasTextContaining('New TODO');
-    assert.dom('.modal').doesNotContainText('Edit TODO');
+    assert.dom('.modal').hasTextContaining('New TODO')
+    assert.dom('.modal').doesNotContainText('Edit TODO')
   })
 
-  test('rendered title is "New TODO when the item is new record"', async function (this: Context, assert) {
+  test('rendered title is "Edit TODO when the item is saved record"', async function (this: Context, assert) {
     const store = this.owner.lookup('service:store')
     store.push({
       data: [
@@ -89,22 +103,31 @@ module("Integration | Component | todo-item/form-modal", function (hooks) {
     }
     this.item = item
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+    await render(
+      hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`)
+    )
 
-    assert.dom('.modal').doesNotContainText('New TODO');
-    assert.dom('.modal').hasTextContaining('Edit TODO');
+    assert.dom('.modal').doesNotContainText('New TODO')
+    assert.dom('.modal').hasTextContaining('Edit TODO')
   })
 
   test('Save record when save button clicked', async function (this: Context, assert) {
     const store = this.owner.lookup('service:store')
-    const item = store.createRecord('todo-item', { name: 'new item!', body: 'new item body!' })
+    const item = store.createRecord('todo-item', {
+      name: 'new item!',
+      body: 'new item body!',
+    })
     this.item = item
     this.close = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    await render(hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`))
+    await render(
+      hbs(`<TodoItem::FormModal @item={{this.item}} @close={{this.close}}/>`)
+    )
 
-    assert.dom('.modal').isVisible();
+    assert.dom('.modal').isVisible()
     const buttons = findAll('.modal button')
-    const saveButton = buttons.find((button) => button.textContent?.trim() === 'Save')
+    const saveButton = buttons.find(
+      (button) => button.textContent?.trim() === 'Save'
+    )
     if (!saveButton) {
       throw new Error('saveButton is null')
     }
@@ -115,16 +138,16 @@ module("Integration | Component | todo-item/form-modal", function (hooks) {
           ctx.status(201),
           ctx.set('Content-Type', 'application/vnd.api+json'),
           ctx.json({
-            data:              {
-                type: 'todo-item',
-                id: 1,
-                attributes: {
-                  name: 'new item!',
-                  is_completed: true,
-                  created_at: '2021-01-01T00:00:00.000Z',
-                  updated_at: '2021-01-01T00:00:00.000Z',
-                },
+            data: {
+              type: 'todo-item',
+              id: 1,
+              attributes: {
+                name: 'new item!',
+                is_completed: true,
+                created_at: '2021-01-01T00:00:00.000Z',
+                updated_at: '2021-01-01T00:00:00.000Z',
               },
+            },
           })
         )
       })
